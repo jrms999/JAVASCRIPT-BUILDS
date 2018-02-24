09698b4e51d7d0ff6964e0c8a7b7400e57ce5ef0
John Sutherland <jrms999@gmail.com>
2018-02-24 17:05:30 +0000

    Add files via upload

---
+++ whats your name - prompt/js/main.js
@@ -0,0 +1,33 @@
+//var testOne = "What kind of variable is this?";
+//var testTwo = 256;
+//var testThree = ["What", "kind", "of", "variable", "am", "I?"];
+//var testFour = false;
+//var testFive = [1, 2, 5];
+//var testSix = 128;
+//var testSeven = "What am I?"
+//var testEight = true;
+//var testNine = ["What kind of variable is this?", 2, 5, 19];
+
+var username = prompt ("What's your name?")
+	//document.write(username)
+if (username == "alice" || username == "Bob"){
+	document.write("Hello " + username + ", how you do'in?")
+} else {
+	document.write("Go away! " + username + " ! ")
+}
+document.write(username)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
